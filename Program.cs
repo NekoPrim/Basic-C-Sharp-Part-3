06b9f15604bd764e7c915cbcbc5f31c206a98e40
Desiree Quade <nekodez@gmail.com>
2022-04-04 20:50:19 -0500

    added README

---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿Console.WriteLine("****** Part Supply *******");
+// REQUIRED FEATURES
+
+// 1. Create a variable called 'partsNeeded' with a value of the number 40
+//    & console.log the variable
+Console.WriteLine("1. Number of partsNeeded:");
+
+// let partsNeeded=40;
+
+// console.log(partsNeeded);
+
+// 2. Create a variable call 'supplyChanges' set it to an array containing
+//    the following numbers: 3, 5, -6, 0, 7, 11
+// console.log('2. Array of supplyChanges:');
+
+// let supplyChanges=[3, 5, -6, 0, 7, 11];
+
+// console.log(supplyChanges);
+
+// 3. Console log the value of the second item in the 'supplyChanges' array
+// console.log('3. Second supplyChange is:');
+
+// console.log(supplyChanges[1]);
+
+// 4. The last item was added by mistake. Remove it from the 'supplyChanges'
+//    array & console.log the value removed.
+// console.log('4. Removed item:');
+
+// let removedS=supplyChanges.pop();
+
+// console.log(removedS);
+
+// 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
+// console.log('5. Adding 25 to supplyChanges.');
+
+// supplyChanges.push(25);
+
+// console.log(supplyChanges);
+
+// 6. Write a `for` loop that shows each value in the 'supplyChanges' array
+//    Use a console.log formatted as follows, where x is the value from the array
+//    - if it is a positive number (greater than 0), log 'Added x parts.'
+//    - if the value is 0, log 'No Change.'
+//    - if the value is negative, format the log as 'Removed x parts.'
+// console.log('6. Showing supplyChanges...');
+
+// let x=0;
+
+// for (x in supplyChanges){
+//   if (supplyChanges[x] > 0){
+//     console.log('Add', supplyChanges[x], 'parts');
+//   }
+//   if (supplyChanges[x] === 0){
+//     console.log('No Change', supplyChanges[x]);
+//   }
+//   if (supplyChanges[x] < 0){
+//     console.log('Remove', supplyChanges[x], 'parts');
+//   }
+// }
+
+// STRETCH GOALS
+// console.log('---  Stretch Goals  ---');
+// // 7. Rewrite the `for` loop from #6 as a `for of` loop.
+// console.log('7. Showing supplyChanges with "for of" loop');
+
+// x=0;
+
+// for (x of supplyChanges){
+//   if (x > 0){
+//     console.log('add', x, 'parts');
+//   }
+//   if (x === 0){
+//     console.log('No Change', x);
+//   }
+//   if (x < 0){
+//     console.log('Remove', x, 'parts');
+//   }
+// }
+
+// 8. Rewrite the `for` loop from #6 as a `while` loop.
+// console.log('8. Showing supplyChanges with "while" loop');
+
+// x=0;
+// let length= supplyChanges.length;
+
+// while (x < length){
+//   if (supplyChanges[x] > 0){
+//     console.log('add', supplyChanges[x], 'parts');
+//   }
+//   if (supplyChanges[x] === 0){
+//     console.log('No Change', supplyChanges[x]);
+//   }
+//   if (supplyChanges[x] < 0){
+//     console.log('Remove', supplyChanges[x], 'parts');
+//   }
+//   x ++;
+// }
+
+// // 9. Write a loop to determine the total number of parts available by
+// //    adding up all the numbers in the 'supplyChanges' array.
+// console.log('9. Total supplies available is:');
+
+// let sum=0;
+
+// for (x= 0; x < length; x ++){
+//   sum += supplyChanges[x];
+// }
+
+// console.log(sum);
+
+
+//my stuff
+
+// $(document).ready(function() {
+//   $(".control").on('click', 'span', function() {
+//     $(".foto img").removeClass("turkey");
+
+//     var newImage = $(this).index();
+
+//     $(".foto img").eq(newImage).addClass("turkey");
+
+//     $(".control span").removeClass("selected");
+//     $(this).addClass("selected");
+//   });
+// });
